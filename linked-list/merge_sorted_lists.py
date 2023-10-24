43563640b3d9039537d9ebf9aa58f14f9faacf82
jsparadacelis <parada.celis@gmail.com>
2023-10-23 22:09:04 -0500

    Linked lists: first versions

--- linked-list/merge_sorted_lists.py
+++ linked-list/merge_sorted_lists.py
@@ -4,37 +4,35 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    
-    def __lt__(self, other: __build_class__):
-        return self.val < other.val
-        
 
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
 
         dummy = ListNode()
-        head = dummy
-        while list1 and list2:
+        curr = dummy
 
+        tail = ListNode()
+        head = tail
+        while list1 and list2:
             if list1.val < list2.val:
-                dummy.next = list1
+                curr.next = list1
                 list1 = list1.next
             else:
-                dummy.next = list2
+                curr.next = list2
                 list2 = list2.next
-            dummy = dummy.next
+            curr = curr.next
         
         if list1:
-            dummy.next = list1
-        elif list2:
-            dummy.next = list2
-        return head.next
+            curr.next = list1
+        else:
+            curr.next = list2
+        return dummy.next
 
 
 if __name__ == "__main__":
-    list1 = ListNode(2, ListNode(5, ListNode(7)))
-    list2 = ListNode(3, ListNode(11))
+
     solution = Solution()
+    list1 = ListNode(1, ListNode(3, ListNode(5)))
+    list2 = ListNode(2, ListNode(3, ListNode(7, ListNode(8))))
     solution.mergeTwoLists(list1=list1, list2=list2)
